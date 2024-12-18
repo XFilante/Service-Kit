d72b8b418a06be0cb77313cb29696a4ce933ee8e
mohitxskull <mohitxskull@gmail.com>
2024-12-18 15:55:09 +0530

    release: 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @filante/service-kit
 
+## 0.0.3
+
+### Patch Changes
+
+- added more options to pivot helper
+
 ## 0.0.2
 
 ### Patch Changes
--- src/helpers/pivot.ts
+++ src/helpers/pivot.ts
@@ -1,10 +1,23 @@
 export const pivot = (
   tableName: string,
   options?: {
-    columns?: string[]
+    pivotColumns?: string[]
+    pivotTable?: string
+    localKey?: string
+    pivotForeignKey?: string
+    relatedKey?: string
+    pivotRelatedForeignKey?: string
+    pivotTimestamps?:
+      | boolean
+      | {
+          createdAt: string | boolean
+          updatedAt: string | boolean
+        }
+    serializeAs?: string | null
+    meta?: any
   }
 ) => ({
   pivotTable: tableName,
   pivotTimestamps: true,
-  pivotColumns: options?.columns,
+  ...options,
 })
